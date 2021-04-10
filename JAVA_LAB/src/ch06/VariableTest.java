3e8e2ebf3991ecfda3c57e86babf83cd39004bb9
Kimjunho <widn45@naver.com>
2021-04-11 00:34:22 +0900

    day2

---
+++ JAVA_LAB/src/ch06/VariableTest.java
@@ -0,0 +1,16 @@
+package ch06;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		byte bnum = -127;//-128~127까지
+		System.out.println(bnum);
+		
+		//int num = 12345678900;//에러 너가쓴 리터럴이 num에 들어가기 크다.
+		//long lNum = 12345678900;//이거 또한 에러 리터럴이 너무 크다.
+		long lNum = 12345678900L;
+		//작을경우에는 자동으로 int형으로 형변환이 된다
+		System.out.println(lNum);
+	}
+
+}
